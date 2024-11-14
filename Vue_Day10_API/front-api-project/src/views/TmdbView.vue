3056b881e65059c625b2560594e8a328b9ab9873
KM <krkkmin1@gmail.com>
2024-11-14 11:10:38 +0900

    day10_Vue_API

---
+++ Vue_Day10_API/front-api-project/src/views/TmdbView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>TMDBView</h2>
+    <router-link :to="{name: 'tmdbPopular'}">인기순위</router-link> | 
+    <router-link :to="{name: 'tmdbTopRated'}">역대순위</router-link>
+    <hr>
+    <router-view/>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
