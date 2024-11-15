0b896fe6204f4d30dc2d9f41b54c11223c6b7c75
강민 <krkkmin1@gmail.com>
2024-11-15 11:30:53 +0900

    vue_day11_YoutubeAPI

---
+++ Vue_Day11_PJT/front-api-project/src/components/youtube/YoutubeVideoList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h4>검색 결과</h4>
+        <YoutubeVideoListItem
+            v-for="video in store.videoList"
+            :key="video.id.videoId"
+            :video="video"
+            >
+
+        </YoutubeVideoListItem>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useYoutubeStore } from '@/stores/youtube';
+import YoutubeVideoListItem from './YoutubeVideoListItem.vue';
+const store = useYoutubeStore()
+</script>
+
+<style  scoped>
+
+</style>
---
+++ Vue_Day11_PJT/front-api-project/src/views/YoutubeView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Youtube View</h2>
+        <!-- 검색 -->
+        <YoutubeVideoSearch/>
+
+        <!-- 상세보기 -->
+        <hr>
+        <YoutubeVideoDetail/>
+
+        <!-- 리스트 -->
+        <hr>
+        <YoutubeVideoList/>
+    </div>
+</template>
+
+<script setup>
+    import YoutubeVideoDetail from '@/components/youtube/YoutubeVideoDetail.vue';
+import YoutubeVideoList from '@/components/youtube/YoutubeVideoList.vue';
+import YoutubeVideoSearch from '@/components/youtube/YoutubeVideoSearch.vue';
+</script>
+
+<style  scoped>
+
+</style>
